153e41b670516502ad6fb25da996f18a76c9c718
jiyoun <kj00kj@zinnaworks.com>
2022-02-21 10:09:38 +0900

    [lgy : 어드민 페이지 누락건 커밋]

---
+++ src/main/java/com/zinnaworks/vo/Group.java
@@ -0,0 +1,13 @@
+package com.zinnaworks.vo;
+
+import lombok.Data;
+
+@Data
+public class Group {
+	
+	private String grpCd; // 부서코드
+	private String grpNm; // 부서명
+	private String preGrpCd; // 상위부서코드
+	private String preGrpNm; // 상위부서명
+	
+}
